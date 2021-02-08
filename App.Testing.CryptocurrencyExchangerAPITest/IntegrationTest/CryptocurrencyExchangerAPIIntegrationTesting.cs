5aa85730b663483a800e1eca924e675247701714
Mouayad Khashfeh <mou2ayad@gmail.com>
2021-02-09 04:51:21 +0800

    Create IntegrationTest

---
+++ App.Testing.CryptocurrencyExchangerAPITest/IntegrationTest/CryptocurrencyExchangerAPIIntegrationTesting.cs
@@ -0,0 +1,73 @@
+﻿using App.Components.Contracts.Models;
+using App.Components.Utilities.ErrorHandling;
+using App.Components.Utilities.JWT_Auth;
+using CryptocurrencyExchanger;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace App.Testing.CryptocurrencyExchangerAPITest.IntegrationTest
+{
+    public class CryptocurrencyExchangerControllerIntegrationTesting : IntegrationTest
+    {
+        
+        public CryptocurrencyExchangerControllerIntegrationTesting():base()
+        {
+            
+        }
+        
+
+        [Fact]
+        public async Task Testing_Quotes_With_Valid_InputAsync()
+        {
+            //Arrange
+            string currencyInput = "BTC";
+            
+            await Authentication("knab","knab2021");
+            var request = new HttpRequestMessage(HttpMethod.Get, $"api/v1/cryptocurrency/Quotes/{currencyInput}");
+
+            //Act
+            var response = await httpClient.SendAsync(request);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var exchangeRatesList = JsonConvert.DeserializeObject<ExchangeRatesList>(responseString);
+
+            //Assert
+            response.IsSuccessStatusCode.Should().BeTrue();
+            exchangeRatesList.Should().NotBeNull();
+            exchangeRatesList.BaseCurrencySymbol.Should().Be("BTC");
+            exchangeRatesList.CurrenciesRates.Should().HaveCountGreaterThan(0);
+            exchangeRatesList.CurrenciesRates.Should().ContainKey("USD");
+
+        }
+
+        [Fact]
+        public async Task Testing_Quotes_With_Invalid_InputAsync()
+        {
+            //Arrange
+            string currencyInput = "SYB";
+            await Authentication("knab", "knab2021");
+            var request = new HttpRequestMessage(HttpMethod.Get, $"api/v1/cryptocurrency/Quotes/{currencyInput}");
+
+            //Act
+            var response = await httpClient.SendAsync(request);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var ErrorMessage = JsonConvert.DeserializeObject<HttpExceptionDetails>(responseString);
+
+            //Assert
+            response.IsSuccessStatusCode.Should().BeFalse();
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            ErrorMessage.StatusCode.Should().Be(400);
+            ErrorMessage.ErrorMessage.Should().Be("SYB is invalid or Unsupported Cryptocurrency");           
+
+        }
+    }
+}
---
+++ App.Testing.CryptocurrencyExchangerAPITest/IntegrationTest/IntegrationTest.cs
@@ -0,0 +1,48 @@
+﻿using App.Components.Utilities.JWT_Auth;
+using CryptocurrencyExchanger;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Testing.CryptocurrencyExchangerAPITest.IntegrationTest
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient httpClient;
+        public IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            httpClient = appFactory.CreateClient();
+        }
+        protected async Task Authentication(string UserName, string Password)
+        {
+            string token;
+            using (var request = new HttpRequestMessage(HttpMethod.Post, "api/authentication/token"))
+            {
+                AuthenticateRequest authenticateRequest = new AuthenticateRequest() { Password = Password, Username = UserName };
+                var json = JsonConvert.SerializeObject(authenticateRequest);
+                using (var stringContent = new StringContent(json, Encoding.UTF8, "application/json"))
+                {
+                    request.Content = stringContent;
+                    var response = await httpClient.SendAsync(request);
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var authResponse = JsonConvert.DeserializeObject<AuthenticateResponse>(responseString);
+                        token = authResponse.Token;
+                        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+                        return;
+                    }
+                    throw new Exception("Unauthorized");
+                }
+            }
+
+        }
+
+    }
+}
